c5311f85e5b23104ee840125adbb2aed89709d46
SUNIL_YANG <avap90@gmail.com>
2018-01-31 02:30:46 +0900

    7번

--- HelloWorld.go
+++ HelloWorld.go
@@ -7,7 +7,7 @@ import (
 func main() {
 
 	for i := 0; i <= 100; i++ {
-		println("Hell o World  !!! Golang St art!!  _6;", i)
+		println("Hell o World  !!! Golang St art!!  _7;", i)
 		time.Sleep(50 * time.Millisecond)
 	}
 }

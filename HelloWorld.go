c3dad09388e8f35043ab301118d7cd727b370a4b
SUNIL_YANG <avap90@gmail.com>
2019-04-30 10:03:43 +0900

    dd test 2

--- HelloWorld.go
+++ HelloWorld.go
@@ -1,12 +1,16 @@
 package main
 
 import (
+	"fmt"
 	"time"
 )
 
 func main() {
 	for i := 0; i <= 100; i++ {
-		println("Hell o World  !!! Golang333rr Start!!;", i)
+		println("Hell o World  !!! Golang333rrc444444d Start!!;", i)
 		time.Sleep(10 * time.Millisecond)
 	}
+
+	fmt.Println("머징???")
+	fmt.Println("응 아니야~~^-^;;")
 }

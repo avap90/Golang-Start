7c1724eee1d37182b4d2301565ece262aaaea15a
SUNIL_YANG <avap90@gmail.com>
2019-05-01 08:52:41 +0900

    runtime 사용,NumCPU에서 ()를 사용하지 않으면 안된다.

--- HelloWorld.go
+++ HelloWorld.go
@@ -1,13 +1,17 @@
 package main
 
 import (
+	"fmt"
+	"runtime"
 	"time"
 )
 
 func main() {
 	for i := 0; i <= 100; i++ {
 		println("Hell o World  !!! Golang333rrc444444d Start!!;", i)
-		time.Sleep(10 * time.Millisecond)
+		time.Sleep(1 * time.Millisecond)
 	}
+
+	fmt.Println("cpu count = ", runtime.NumCPU())
 
 }

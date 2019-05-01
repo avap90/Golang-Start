091433013aee13bbc3649dbe75c05f0bb7af234d
SUNIL_YANG <avap90@gmail.com>
2019-05-01 09:39:46 +0900

    int형 printf 로 출력 그리고 case의 조건문

--- HelloWorld.go
+++ HelloWorld.go
@@ -2,16 +2,49 @@ package main
 
 import (
 	"fmt"
+	"io/ioutil"
+	"math/rand"
 	"runtime"
 	"time"
 )
 
 func main() {
-	for i := 0; i <= 100; i++ {
+	var b []byte
+	var err error
+
+	rand.Seed(time.Now().UnixNano())
+	rand := rand.Intn(100)
+
+	for i := 0; i <= 10; i++ {
 		println("Hell o World  !!! Go ", i)
 		time.Sleep(1 * time.Millisecond)
 	}
 
 	fmt.Println("cpu count = ", runtime.NumCPU())
+
+	b, err = ioutil.ReadFile("./test.txt")
+
+	if err == nil {
+		fmt.Printf("%s", b)
+	}
+
+	if b, err := ioutil.ReadFile("./test.txt"); err == nil {
+		fmt.Printf("%s", b)
+	}
+
+	fmt.Println(rand)
+
+	for i := 0; i < 100; i++ {
+
+		switch {
+		case i%3 == 0 && i%5 == 0:
+			fmt.Printf("%d = 3과 5의 공배수\r\n", i)
+		case i%3 == 0:
+			fmt.Printf("%d = 3의배수\r\n", i)
+		case i%5 == 0:
+			fmt.Printf("%d =5의 배수\r\n", i)
+
+		}
+	}
 
 }
